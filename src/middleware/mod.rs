403190279ad89272d705fcbaf64a625350770d9a
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-06-23 20:41:09 +0300

    added pagination

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -6,14 +6,14 @@ mod request_id;
 mod context;
 mod idempondency;
 mod rate_limiter;
+use crate::Authority;
 pub use auth::Auth;
 pub use constant_time::ResponseEqualizer;
 pub use context::{Context, GetId, ReadContext};
-pub use fns::{authority, identity};
+pub use fns::{authority, identity, attach_pagination, Pagination};
 pub use idempondency::Idempotency;
 pub use rate_limiter::RateLimiter;
 pub use request_id::{RequestId, RequestIdStr};
-use crate::Authority;
 use uuid::Uuid;
 impl GetId for Authority {
     fn get_id(&self) -> Uuid {

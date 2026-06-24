64d48915892854c78b7c93d138f0ca4bbf7b95c1
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-06-24 07:57:39 +0300

    moved pagination to a standard middleware

--- src/middleware/fns.rs
+++ src/middleware/fns.rs
@@ -1,15 +1,13 @@
 use crate::auth::Auth;
 use crate::{Authority, Identity};
-use actix_web::web::Query;
+
 use actix_web::{
     Error, HttpResponse,
     body::BoxBody,
     dev::{ServiceRequest, ServiceResponse},
     middleware::Next,
 };
-use serde::Deserialize;
 use std::pin::Pin;
-use tokio::task_local;
 
 pub async fn identity(
     mut req: ServiceRequest,
@@ -38,54 +36,4 @@ pub fn authority(
             }
         })
     }
-}
-
-task_local! {
-    static PAGINATION: Pagination;
-}
-
-#[derive(Clone, Copy)]
-pub struct Pagination {
-    pub page: u32,
-    pub limit: u32,
-}
-
-impl Default for Pagination {
-    fn default() -> Pagination {
-        Pagination {
-            page: 0,
-            limit: 100,
-        }
-    }
-}
-
-impl Pagination {
-    pub fn get() -> Pagination {
-        PAGINATION.try_with(|p| *p).unwrap_or_default()
-    }
-}
-
-#[derive(Deserialize)]
-struct SafePagination {
-    pub page: Option<u32>,
-    pub limit: Option<u32>,
-}
-
-impl SafePagination {
-    fn get(&self) -> Pagination {
-        Pagination {
-            page: self.page.unwrap_or(0),
-            limit: self.limit.unwrap_or(100),
-        }
-    }
-}
-
-pub async fn attach_pagination(
-    mut req: ServiceRequest,
-    next: Next<BoxBody>,
-) -> Result<ServiceResponse<BoxBody>, Error> {
-    let pagination = req.extract::<Query<SafePagination>>().await?.get();
-    Ok(PAGINATION
-        .scope(pagination, async { next.call(req).await })
-        .await?)
 }
